a756c77aa240c54884be3e5c1cff6dee2d37d799
Melvin Mathew <65242293+mmathewdtx@users.noreply.github.com>
2022-07-15 04:14:05 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,45 +1,29 @@
 'use strict';
-
 var lambdaHandler = function(event, context, callback) {
     const connData = {
         port: event.context['my-port'],
         host: event.context['my-host'],
         timeout: event.context['my-timeout']
     };
-
-
     const timeout = typeof connData.timeout !== 'undefined' ?
                     parseInt(connData.timeout) :
                     50; // Fallback to 50ms
     const net = require('net');
     var message = 'Whoops port ' + connData.port + ' on host `' +
                   connData.host + '` seems closed!';
-
     const client = net.connect(connData, function() {
       message = 'Port ' + connData.port + ' on ' + connData.host + ' is open!';
-
-
       callback(null, message);
-
-
       client.destroy();
     });
-
     client.setTimeout(timeout, function() {
-
         client.destroy(new Error(message), null);
     });
-
     client.on('error', function(err) {
-
         callback(new Error(message), null);
     });
 };
-
-
 exports.checkPortOpen = lambdaHandler;
-
-
 if (require.main === module) {
     var lambdaContext = {
         context: {
@@ -48,7 +32,6 @@ if (require.main === module) {
             'my-timeout': process.env.MY_TIMEOUT
         }
     };
-
     lambdaHandler(lambdaContext, null, function(err, data) {
         if (err) {
             console.log(err.message);
